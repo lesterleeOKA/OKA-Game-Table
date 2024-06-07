2fcc46b599ce3dffbbd0ca67ac1a1d05486383f2
Lester Lee <82643100+lesterleefat0418@users.noreply.github.com>
2024-06-07 18:11:02 +0800

    backup

--- js/script.js
+++ js/script.js
@@ -5,6 +5,20 @@ function updatePhonicsGameLink() {
     const newUrl = `${baseUrl}?unit=${selectedUnit}`;
     window.open(newUrl);
 }
+function updateSnakeGameLink() {
+    const unitPicker = document.getElementById('snake-unit-picker');
+    const selectedUnit = unitPicker.value;
+    const baseUrl = 'https://dev.openknowledge.hk/RainbowOne/webapp/lester/Snake/';
+    const newUrl = `${baseUrl}?unit=${selectedUnit}`;
+    window.open(newUrl);
+}
+function updateTypingGameLink() {
+    const unitPicker = document.getElementById('typing-unit-picker');
+    const selectedUnit = unitPicker.value;
+    const baseUrl = 'https://dev.openknowledge.hk/RainbowOne/webapp/sing/Typing/';
+    const newUrl = `${baseUrl}?unit=${selectedUnit}`;
+    window.open(newUrl);
+}
 function updateSpellingGameLink() {
     const unitPicker = document.getElementById('mspell-unit-picker');
     const selectedUnit = unitPicker.value;
