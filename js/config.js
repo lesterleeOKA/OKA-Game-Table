ab92dd0eed76ffd626a0dde94a4e9a856eaaef5c
Lester Lee <82643100+lesterleefat0418@users.noreply.github.com>
2024-07-11 14:35:38 +0800

    updated to 1.06

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-//export const HEADER="https://www.rainbowone.app/";
-export const HEADER="https://dev.openknowledge.hk/";
+export const HEADER="https://www.rainbowone.app/";
+//export const HEADER="https://dev.openknowledge.hk/";
--- js/script.js
+++ js/script.js
@@ -1,97 +1,57 @@
-import { HEADER } from './config.js';
-
-const RainbowOneUtils = {
-  updatePhonicsGameLink,
-  updateSnakeGameLink,
-  updateTypingGameLink,
-  updateSpellingGameLink,
-  nodoubletapzoom,
-  hideURLPath,
-};
-
-function updatePhonicsGameLink(removalStatus) {
-  console.log("removalStatus", removalStatus);
+function updatePhonicsGameLink() {
     const unitPicker = document.getElementById('mpcard-unit-picker');
     const selectedUnit = unitPicker.value;
-    const baseUrl = HEADER + '/RainbowOne/webapp/OKAGames/Phonics/';
-    const newUrl = `${baseUrl}?unit=${selectedUnit}${removalStatus}`;
+    const baseUrl = 'https://dev.openknowledge.hk/RainbowOne/webapp/lester/phonics/';
+    const newUrl = `${baseUrl}?unit=${selectedUnit}`;
     window.open(newUrl, '_self');
 }
 function updateSnakeGameLink() {
     const unitPicker = document.getElementById('snake-unit-picker');
     const selectedUnit = unitPicker.value;
-    const baseUrl = HEADER + '/RainbowOne/webapp/OKAGames/Snake/';
+    const baseUrl = 'https://dev.openknowledge.hk/RainbowOne/webapp/lester/Snake/';
     const newUrl = `${baseUrl}?unit=${selectedUnit}`;
-    window.open(newUrl, '_self');
+    window.open(newUrl);
 }
 function updateTypingGameLink() {
     const unitPicker = document.getElementById('typing-unit-picker');
     const selectedUnit = unitPicker.value;
-    const baseUrl = HEADER + '/RainbowOne/webapp/OKAGames/Typing/';
+    const baseUrl = 'https://dev.openknowledge.hk/RainbowOne/webapp/sing/Typing/';
+    const newUrl = `${baseUrl}?unit=${selectedUnit}`;
+    window.open(newUrl);
+}
+function updateSpellingGameLink() {
+    const unitPicker = document.getElementById('mspell-unit-picker');
+    const selectedUnit = unitPicker.value;
+    const baseUrl = 'https://dev.openknowledge.hk/RainbowOne/webapp/lester/Motion-Spelling/';
     const newUrl = `${baseUrl}?unit=${selectedUnit}`;
     window.open(newUrl, '_self');
 }
-function updateSpellingGameLink(removalStatus) {
-    console.log("removalStatus", removalStatus);
-    const unitPicker = document.getElementById('mspell-unit-picker');
-    const selectedUnit = unitPicker.value;
-    const baseUrl = HEADER + '/RainbowOne/webapp/OKAGames/MotionSpelling/';
-    const newUrl = `${baseUrl}?unit=${selectedUnit}${removalStatus}`;
-    window.open(newUrl, '_self');
-}
 
- function nodoubletapzoom() {
-  console.log('HEADER:', HEADER);
 
-  function preventZoom(event) {
-    let touchStartTime, lastTouchTime, timeDiff, touches;
-
-    touchStartTime = event.timeStamp;
-    lastTouchTime = this.dataset.lastTouch || touchStartTime;
-    timeDiff = touchStartTime - lastTouchTime;
-    touches = event.touches.length;
-    this.dataset.lastTouch = touchStartTime;
-
-    if (!timeDiff || timeDiff > 500 || touches > 1) return; // not double-tap
-
-    event.preventDefault(); // double tap - prevent the zoom
-    // also synthesize click events we just swallowed up
-    this.click();
-    this.click();
-  }
-
-  const elements = document.querySelectorAll('.game-card .game-image');
-  for (let i = 0; i < elements.length; i++) {
-    elements[i].addEventListener('touchstart', preventZoom);
-  }
-}
-
-function hideURLPath() {
-  const currentURL = new URL(window.location.href);
-  const baseURL = `${currentURL.protocol}//${currentURL.host}/`;
-
-  const hiddenPath = 'RainbowOne/webapp/OKAGames/';
-
-  // Check if the current URL includes the hidden path
-  if (currentURL.pathname.includes(hiddenPath)) {
-    // Store the original URL
-    const originalURL = window.location.href;
-
-    // Update the URL to the base URL
-    window.history.replaceState(null, null, baseURL);
-
-    // Set up the onbeforeunload event handler
-    window.onbeforeunload = (event) => {
-      // Check if the original URL is available
-      if (originalURL) {
-        // Navigate back to the original URL
-        window.location.href = originalURL;
-      }
-    };
-  } else {
-    // Remove the onbeforeunload event handler if the hidden path is not present
-    window.onbeforeunload = null;
-  }
-}
-
-  export default RainbowOneUtils;
+(function() {
+    function nodoubletapzoom() {
+      var touchStartTime, lastTouchTime, timeDiff, touches;
+  
+      this.addEventListener('touchstart', function preventZoom(event) {
+        touchStartTime = event.timeStamp;
+        lastTouchTime = this.dataset.lastTouch || touchStartTime;
+        timeDiff = touchStartTime - lastTouchTime;
+        touches = event.touches.length;
+        this.dataset.lastTouch = touchStartTime;
+  
+        if (!timeDiff || timeDiff > 500 || touches > 1) return; // not double-tap
+  
+        event.preventDefault(); // double tap - prevent the zoom
+        // also synthesize click events we just swallowed up
+        this.click();
+        this.click();
+      });
+    }
+  
+    // Apply the nodoubletapzoom function to the desired elements
+    var elements = document.querySelectorAll('.game-card .game-image');
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].nodoubletapzoom = nodoubletapzoom;
+      elements[i].nodoubletapzoom();
+    }
+  })();
